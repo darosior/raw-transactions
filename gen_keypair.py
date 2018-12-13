4acdfb7bf23eab24075aeffa1faad389c565d28a
darosior@gmail.com <darosior@gmail.com>
2018-12-13 01:06:32 +0100

    First

---
+++ gen_keypair.py
@@ -0,0 +1,190 @@
+# coding: utf-8
+import os
+import uuid
+import time
+import hashlib
+from math import log
+import sys
+sys.path.append('.')
+import secp256k1
+
+
+# From https://github.com/darosior/bitcoineasy/blob/master/bitcoineasy/utils.py
+def sizeof(n):
+    """
+    get the size in bytes of an integer, https://stackoverflow.com/questions/14329794/get-size-of-integer-in-python
+
+    :param n: the integer to get the size from
+    :return: the size in bytes of the int passed as the first parameter.
+    """
+    if n == 0:
+        return 1
+    return int(log(n, 256)) + 1
+
+
+def hash160(bytes, bin=False):
+    """
+    Returns the ripemd160(sha256(data)), used a lot in Bitcoin.
+
+    :param bin: If set to true, returns bytes.
+    """
+    rip = hashlib.new('ripemd160')
+    rip.update(hashlib.sha256(bytes).digest())
+    if bin:
+        return rip.digest()  # type : bytes
+    else:
+        return rip.hexdigest()  # type : str
+
+
+def double_sha256(bytes, bin=False):
+    """
+    Returns the sha256(sha256(data)), used a lot in Bitcoin.
+
+    :param bin: If set to true, returns bytes.
+    """
+    h = hashlib.sha256(bytes)
+    if bin:
+        return hashlib.sha256(h.digest()).digest()  # type : bytes
+    else:
+        return hashlib.sha256(h.digest()).hexdigest()  # type : str
+
+
+def gen_random():
+    """
+    Generates a random number from a CSRNG.
+    """
+    seconds = int(time.time())
+    entrop1 = double_sha256(seconds.to_bytes(sizeof(seconds), 'big'), True)
+    entrop2 = double_sha256(os.urandom(256), True)
+    entrop3 = double_sha256(uuid.uuid4().bytes, True)
+    return double_sha256(entrop1 + entrop2 + entrop3, True)
+
+
+def b58encode(payload):
+    """
+    Takes a number (int or bytes) and returns its base58_encoding.
+
+    :param payload: The data to encode, can be bytes or int
+    :return: the number passed as first parameter as a base58 encoded str.
+    """
+    if isinstance(payload, bytes):
+        n = int.from_bytes(payload, 'big')
+    elif isinstance(payload, int):
+        n = payload
+    else:
+        raise ValueError('b58encode takes bytes or int')
+
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    x = n % 58
+    rest = n // 58
+    if rest == 0:
+        return alphabet[x]
+    else:
+        return b58encode(rest) + alphabet[x]
+
+
+def b58decode(string):
+    """Takes a base58-encoded number and returns it in base10.
+    :param string: the number to base58_decode (as str).
+    :return: the number passed as first parameter, base10 encoded.
+    """
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    # Populating a dictionary with base58 symbol chart
+    dict = {}
+    k = 0
+    for i in alphabet:
+        dict[i] = k
+        k += 1
+    n = 0  # Result
+    pos = 0  # Cf https://www.dcode.fr/conversion-base-n
+    for i in string:
+        for y in alphabet:
+            if i == y:
+                n = n * 58 + dict[i]
+        pos += 1
+    return n
+
+
+def encode_check(payload):
+    """Returns the base58 encoding with a 4-byte checksum.
+
+    :param payload: The data (as bytes) to encode.
+    """
+    checksum = double_sha256(payload, True)[:4]
+    if payload[0] == 0x00:
+        # Again, the leading 0 problem which results in nothing during int conversion
+        return b58encode(b'\x00') + b58encode(payload[1:] + checksum)
+    else:
+        return b58encode(payload + checksum)
+
+
+def decode_check(string):
+    """Returns the base58 decoded value, verifying the checksum.
+
+    :param string: The data to decode, as a string.
+    """
+    number = b58decode(string)
+    # Converting to bytes in order to verify the checksum
+    payload = number.to_bytes(sizeof(number), 'big')
+    if payload and double_sha256(payload[:-4], True)[:4] == payload[-4:]:
+        return payload[:-4]
+    else:
+        return None
+
+
+def wif_encode(data, prefix=b'\x80'):
+    """
+    WIF-encode the data (which would likely be a Bitcoin private key) provided.
+
+    :param data: The bytes to WIF-encode.
+    """
+    return encode_check(prefix + data) # str
+
+
+def wif_decode(string):
+    """
+    WIF-decode the provided string (which would likely be a WIF-encoded Bitcoin private key).
+    """
+    dec = decode_check(string)
+    if string[0] == 'T': # For Bitcoin the condition is if string[0] == 'K' or 'L'
+        return dec[1:-1] # bytes
+    else:
+        return dec[1:] # bytes
+
+
+def gen_privkey():
+    while True:
+        n = int.from_bytes(gen_random(), 'big')
+        if 0 < n < 115792089237316195423570985008687907852837564279074904382605163141518161494337:
+            return n.to_bytes(sizeof(n), 'big')
+
+
+def get_pubkey(privkey):
+    if len(privkey) == 33:
+        (x, y) = secp256k1.privtopub(privkey[:32])
+        return b'\x03' + x.to_bytes(sizeof(x), 'big') if y % 2 else b'\x02' + x.to_bytes(sizeof(x), 'big')
+    else:
+        (x, y) = secp256k1.privtopub(privkey)
+        return b'\x04' + x.to_bytes(sizeof(x), 'big') + y.to_bytes(sizeof(y), 'big')
+
+
+def get_address(pubkey):
+    version_byte = b'\x30'
+    hash = hash160(pubkey, True)
+    return encode_check(version_byte + hash)
+
+
+def get_keypair():
+    privkey = gen_privkey()
+    address = get_address(get_pubkey(privkey))
+    print('Privkey : ', hex(int.from_bytes(privkey, 'big')))
+    print('Address : ', address)
+
+
+if __name__ == '__main__':
+    # get_keypair()
+    import binascii
+    pk = wif_decode('T9z15bnpSN4hoyDx5JvgirFEYoyNUtcCfmSY7Vm3nUUUCwAuAYpD') + b'\x01'
+    print(pk)
+    print(binascii.hexlify(pk))
+    print(binascii.hexlify(get_pubkey(pk)))
---
+++ gen_rawtx.py
@@ -0,0 +1,295 @@
+# coding: utf-8
+import sys
+sys.path.append('.')
+from gen_keypair import *
+import opcodes
+import binascii
+import ecdsa
+import requests
+import time
+import socket
+
+def der_encode(r, s):
+    """
+    DER-encodes a signed tx. https://bitcoin.stackexchange.com/questions/12554/why-the-signature-is-always-65-13232-bytes-long
+    https://github.com/bitcoin/bitcoin/blob/ce74799a3c21355b35fed923106d13a0f8133721/src/script/interpreter.cpp#L108
+    """
+    r_len = sizeof(r)
+    s_len = sizeof(s)
+    total_len = (4 + r_len + s_len) # 5 = 02 + r_len + 02 + s_len (all 1 byte)
+    return b'\x30' + total_len.to_bytes(sizeof(total_len), 'big') + b'\x02' + r_len.to_bytes(sizeof(r_len), 'big') \
+            + r.to_bytes(sizeof(r), 'big') + b'\x02' + s_len.to_bytes(sizeof(s_len), 'big') + s.to_bytes(sizeof(s), 'big')
+
+
+class Bitcoind:
+    """
+    An interface to the Bitcoin daemon (or Insacoin one).
+    """
+    def __init__(self, url, user, password):
+        self.url = url
+        self.session = requests.Session()
+        self.session.auth = (user, password)
+        self.session.headers.update({'content-type' : 'text/plain'})
+
+    def send(self, call, params=[]):
+        """
+        Makes a RPC call to the daemon.
+
+        :param call: The method to send, as string.
+        :param params: The parameters to send with the method.
+        :return: The JSON response.
+        """
+        payload = {'jsonrpc':'1.0',
+                   'id':call,
+                   'method':call,
+                   'params':params}
+        r = self.session.post(self.url, json=payload)
+        return r.json()
+
+class Script:
+    """
+    This class represents a Bitcoin script.
+    """
+    def __init__(self, script):
+        """
+        :param script: The script as a string.
+        """
+        self.script = script
+        self.serialized = self.parse()
+        self.size = len(self.serialized)
+
+    def parse(self):
+        """
+        Parses and serializes a script.
+
+        :return: The serialized script, as bytes.
+        """
+        # Parsing the string
+        instructions = self.script.split(' ')
+        serialized = b''
+        # Filling with the corresponding OPCODEs
+        for i in instructions:
+            if i in opcodes.OPCODE_NAMES:
+                op = opcodes.OPCODE_NAMES.index(i)
+                serialized += op.to_bytes(sizeof(op), 'big')
+            else:
+                # There may be some hex numbers in the script which are not OPCODE
+                try:
+                    value = int(i, 16)
+                    length = sizeof(value)
+                    serialized += length.to_bytes(sizeof(length), 'big') + value.to_bytes(sizeof(value), 'big')
+                except:
+                    raise Exception('Unexpected instruction in script : {}'.format(i))
+        if len(serialized) > 10000:
+            raise Exception('Serialized script should be less than 10,000 bytes long')
+        return serialized
+
+
+class Transaction:
+    """
+    Represents a Bitcoin transaction.
+    For simplicity this transaction just spends one output and creates one input.
+    """
+    def __init__(self, daemon, prev_hash, index, script_sig, value, script_pubkey):
+        """
+        :param daemon: An instance of the Bitcoind class.
+        :param prev_hash: The id of the transaction which contains the output spent by this transaction.
+        :param index: The index of the output spent by this transaction in the output list of the precedent one.
+        :param script_sig: The unlocking script of the output of this transaction.
+        :param value: The value spent from the output.
+        :param script_pubkey: The locking script of the output created by this transaction.
+        """
+        self.network = daemon
+        self.id = None
+        self.serialized = None
+        self.script_sig = script_sig
+        self.script_pubkey = script_pubkey
+        if isinstance(prev_hash, int):
+            self.prev_hash = prev_hash.to_bytes(sizeof(prev_hash), 'big')
+        elif isinstance(prev_hash, bytes):
+            self.prev_hash = prev_hash
+        else:
+            raise Exception('prev_hash must be specified as int or bytes, not {}'.format(type(prev_hash)))
+        if isinstance(index, int):
+            self.index = index.to_bytes(4, 'little', )
+        elif isinstance(index, bytes):
+            self.index = index
+        else:
+            raise Exception('index must be specified as int or bytes, not {}'.format(type(index)))
+        if isinstance(value, int):
+            self.value = value.to_bytes(8, 'little')
+        elif isinstance(value, bytes):
+            self.value = value
+        else:
+            raise Exception('value must be specified as int or bytes, not {}'.format(type(value)))
+
+    def serialize(self, script_sig=None):
+        """
+        Serializes the transaction.
+        :return: The serialized transaction, as bytes.
+        """
+        if not script_sig:
+            script_sig = self.script_sig
+        tx = b'\x01\x00\x00\x00'  # version
+        tx += b'\x01'  # input count
+        tx += self.prev_hash[::-1]
+        tx += self.index
+        script_length = len(script_sig)
+        tx += script_length.to_bytes(sizeof(script_length), 'big')
+        tx += script_sig
+        tx += b'\xff\xff\xff\xff'  # sequence
+        tx += b'\x01'  # output count
+        tx += self.value
+        script_length = len(self.script_pubkey)
+        tx += script_length.to_bytes(sizeof(script_length), 'big')
+        tx += self.script_pubkey
+        tx += b'\x00\x00\x00\x00'  # timelock
+        self.serialized = tx
+        return binascii.hexlify(tx)
+
+    def print(self):
+        """
+        Displays the decoded transaction in a JSON-like way.
+        This method is quite messy. Actually, this function IS messy.
+        """
+        assert self.serialized is not None
+        tx = self.serialized
+        print('{')
+        print(' version : ', binascii.hexlify(tx[:4]), ',')
+        print(' input_count : ', tx[4], ',')
+        print(' prev_hash : ', binascii.hexlify(tx[5:37]), ',')
+        print(' index : ', binascii.hexlify(tx[37:41]), ',')
+        scriptsig_len = tx[41]
+        print(' scriptsig_len : ', scriptsig_len, ',')
+        print(' scriptsig : ', binascii.hexlify(tx[42:42 + scriptsig_len]), ',')
+        print(' sequence', binascii.hexlify(tx[42 + scriptsig_len:42 + scriptsig_len + 4]), ',')
+        print(' output_count', tx[42 + scriptsig_len + 4], ',')
+        print(' value : ', binascii.hexlify(tx[42 + scriptsig_len + 4:42 + scriptsig_len + 12]), ',')  # aie aie aie
+        output_length = tx[42 + scriptsig_len + 13]
+        print(' output_length : ', output_length, ',')
+        print(' output : ', binascii.hexlify(tx[42 + scriptsig_len + 14:42 + scriptsig_len + 13 + output_length + 1]),
+              ',')  # ouie
+        print(' locktime : ',
+              binascii.hexlify(tx[42 + scriptsig_len + 13 + output_length + 1:42 + scriptsig_len + output_length + 18]),
+              ',')
+        print('}')
+
+    def get_prev_pubkey(self):
+        """
+        Fetches the script_pubkey from the ouput spent by this tx.
+
+        :return: The script as bytes.
+        """
+        txid = hex(int.from_bytes(self.prev_hash, 'big'))[2:]
+        index = int.from_bytes(self.index, 'little')
+        return binascii.unhexlify(self.network.send('getrawtransaction', [txid, 1])['result']['vout'][index]['scriptPubKey']['hex'])
+
+    def sign(self, key):
+        """
+        Signs the transaction.
+
+        :param key: The private key with which to sign the transaction.
+        :return: The DER-encoded signature.
+        """
+        # To sign the transaction, we serialize it with the script_sig being the script_pubkey of the output spent.
+        tx = binascii.unhexlify(self.serialize(script_sig=self.get_prev_pubkey()))
+        # Then we hash this serialized transaction, giving us the payload to sign
+        tx_hash = double_sha256(tx + b'\x01\x00\x00\x00', True) # + the hash_code byte
+        secexp = int.from_bytes(key, 'big')
+        sk = ecdsa.SigningKey.from_secret_exponent(secexp, curve=ecdsa.SECP256k1)
+        # The byte appended is the hash_code byte, signifying we will use SIGHASH_ALL
+        sig = sk.sign_digest(tx_hash, sigencode=ecdsa.util.sigencode_der_canonize) + b'\x01'
+        return sig
+
+    def create_and_sign(self, privkey, pubkey):
+        """
+        Creates a raw transaction and signs it.
+
+        :param privkey: The key to sign the tx with.
+        :param pubkey: The corresponding public key.
+        :return: A serialized and signed Bitcoin transaction.
+        """
+        # self.sign creates the raw tx so we don't have to do it before
+        sig = self.sign(privkey)
+        # We build the final script_sig
+        sig_len = len(sig)
+        pub_len = len(pubkey)
+        script_sig = sig_len.to_bytes(sizeof(sig_len), 'big') + sig + pub_len.to_bytes(sizeof(pub_len), 'big') + pubkey
+        return self.serialize(script_sig=script_sig)
+
+    def send(self):
+        """
+        Sends the transaction to the network.
+        """
+        # Monkey patching of hex() erasing leading 0s
+        tx = '0' + hex(int.from_bytes(self.serialized, 'big'))[2:]
+        response = self.network.send('sendrawtransaction', params=[tx])
+        if not response['error']:
+            self.id = response['result']
+            return True
+        else:
+            return response['error']
+
+    def send_the_hard_way(self, ip):
+        """
+        Sends a transaction to the network without using RPC, just a raw network message.
+        https://en.bitcoin.it/wiki/Protocol_documentation
+
+        :param ip: The node to which send the message. A string.
+        """
+        # First the version message
+        # https://en.bitcoin.it/wiki/Version_Handshake
+        magic = 0xddb8c2fd.to_bytes(4, 'little')
+        version = int(70003).to_bytes(4, 'little')
+        services = int(1).to_bytes(8, 'little')
+        timestamp = int(time.time()).to_bytes(8, 'little')
+        myip = socket.inet_aton(requests.get('https://api.ipify.org').text)
+        nodeip = socket.inet_aton(ip)
+        # 7333 -> insacoin
+        addr_recv = services + b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff' + myip + int(7333).to_bytes(2, 'big')
+        addr_from = services + b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff' + nodeip + int(7333).to_bytes(2, 'big')
+        nonce = 0x00.to_bytes(8, 'little')
+        user_agent = 0x00.to_bytes(1, 'big')
+        start_height = 0x00.to_bytes(4, 'little')
+        payload = version + services + timestamp + addr_recv + addr_from + nonce + user_agent + start_height
+        checksum = double_sha256(payload, bin=True)[:4]
+        payload_length = len(payload)
+        # NULL padded ascii command
+        version_message = magic + 'version'.encode('ascii') + b'\x00\x00\x00\x00\x00' + payload_length.to_bytes(4, 'little') + checksum + payload
+        # Now the tx message
+        checksum = double_sha256(self.serialized, bin=True)[:4]
+        tx_length = len(self.serialized)
+        tx_message = magic + 'tx'.encode('ascii') + b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' + tx_length.to_bytes(4, 'little') + checksum + self.serialized
+        # Now the verack message
+        checksum = double_sha256(b'', bin=True)[:4]
+        verack_message = magic + 'verack'.encode('ascii') + b'\x00\x00\x00\x00\x00\x00' + 0x00.to_bytes(4, 'little') + checksum
+        # Now let's connect to the node and send it our messages
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, 7333))
+        s.send(version_message)
+        s.recv(1000) # receive version + verack
+        s.send(verack_message)
+        s.send(tx_message)
+        #print('Error message : ')
+        #print(s.recv(1000))
+
+
+if __name__ == '__main__':
+    """
+    privkey = wif_decode('T41pKW96rRf9qfMTUeADvfiikjH2hfBkMvcL72vRjGNrHU1urucv')
+    pubkey = get_pubkey(privkey + b'\x01') # + the compression byte)
+    prev_txid = 0xef3c5c5af079c4231227ad2a608afc54e2ca4b440f1942887b84bfafe726ae47
+    scriptpubkey = Script('OP_RETURN 6461726f73696f72')
+    index = 0
+    value = 0 # In Satoshis
+
+    myCoin = Bitcoind('http://127.0.0.1:7332', 'insacoinrpc', '5CR4JLMSMVspaq8odQH543nJHQ5RX4r5h6Sw9XRp5oL6')
+    myTransaction = Transaction(myCoin, prev_txid, index, script_sig=None, value=value, script_pubkey=scriptpubkey.parse())
+    myTransaction.create_and_sign(privkey, pubkey)
+    myTransaction.print()
+    print(myTransaction.send())
+    """
+    privkey = wif_decode('T9z15bnpSN4hoyDx5JvgirFEYoyNUtcCfmSY7Vm3nUUUCwAuAYpD')
+    pubkey = get_pubkey(privkey + b'\x01')  # + the compression byte)
+    print(binascii.hexlify(privkey))
+    print(get_address(pubkey))
